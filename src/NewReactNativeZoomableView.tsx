4404986a79add0ca6b133d6c4e848e0d2fa6beb1
thomasvo <thomasttvo@gmail.com>
2022-01-28 14:14:37 -0800

    made zoom work for X axis and pinchX always 0

--- src/NewReactNativeZoomableView.tsx
+++ src/NewReactNativeZoomableView.tsx
@@ -1,103 +1,115 @@
 import React, { useState } from 'react';
+import { View } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
 } from 'react-native-reanimated';
 
-export const NewReactNativeZoomableView = ({
-  children,
-  // contentWidth,
-  // contentHeight,
-}) => {
-  const [layout, setLayout] = useState({ height: 10, width: 10 });
+export const NewReactNativeZoomableView = ({ children }) => {
+  const [layout, setLayout] = useState({ height: 0, width: 0 });
 
-  const dragOffset = { x: useSharedValue(0), y: useSharedValue(0) };
-  const dragStart = { x: useSharedValue(0), y: useSharedValue(0) };
-  const dragGesture = Gesture.Pan()
-    .averageTouches(true)
-    .maxPointers(1)
-    .onBegin(() => {})
-    .onUpdate((e) => {
-      'worklet';
-      dragOffset.x.value = e.translationX + dragStart.x.value;
-      dragOffset.y.value = e.translationY + dragStart.y.value;
-    })
-    .onEnd(() => {
-      'worklet';
-      dragStart.x.value = dragOffset.x.value;
-      dragStart.y.value = dragOffset.y.value;
-    });
+  const pinchStart = { x: useSharedValue(0) };
+  const eventScale = useSharedValue(1);
 
-  const pinchStart = { x: useSharedValue(0), y: useSharedValue(0) };
-  const pinchPosition = { x: useSharedValue(0), y: useSharedValue(0) };
-  const pinchTransform = { x: useSharedValue(0), y: useSharedValue(0) };
-  const scale = useSharedValue(1);
-
-  const savedScale = useSharedValue(1);
-  const savedPinchTransform = { x: useSharedValue(0), y: useSharedValue(0) };
+  const last = {
+    x: useSharedValue(0),
+    scale: useSharedValue(1),
+    width: useSharedValue(300),
+  };
 
   const zoomGesture = Gesture.Pinch()
     .onBegin((event) => {
       'worklet';
-
       // Get the point where we started pinching
       pinchStart.x.value = event.focalX;
-      pinchStart.y.value = event.focalY;
-
-      // TODO: add previous transform here
     })
     .onUpdate((event) => {
       'worklet';
-
-      pinchPosition.x.value = event.focalX;
-      pinchPosition.y.value = event.focalY;
-
-      pinchTransform.x.value = event.focalX - pinchStart.x.value;
-      pinchTransform.y.value = event.focalY - pinchStart.y.value;
-
-      scale.value = savedScale.value * event.scale;
+      eventScale.value = event.scale;
     })
     .onEnd(() => {
       'worklet';
+      const oldScale = last.scale.value;
+      const newScale = oldScale * eventScale.value;
 
-      savedScale.value = scale.value;
-      // Save previous transform
-      // TODO: add it to start of next transform
-      savedPinchTransform.x.value = pinchTransform.x.value;
-      savedPinchTransform.y.value = pinchTransform.y.value;
+      const oldWidth = layout.width * oldScale;
+      const newWidth = layout.width * newScale;
+
+      last.scale.value = newScale;
+      last.width.value = newWidth;
+      last.x.value = last.x.value - oldWidth / 2 + newWidth / 2;
+      eventScale.value = 1;
     });
 
   const animatedStyles = useAnimatedStyle(() => {
-    const { width, height } = layout;
-    const pinchX = pinchStart.x.value / width;
-    const pinchY = pinchStart.y.value / height;
+    let { width } = layout;
+    if (!width) return {};
+
+    const lastX = last.x.value;
+    const lastScale = last.scale.value;
+    const scaledWidth = width * lastScale;
 
     return {
       transform: [
-        { translateX: dragOffset.x.value / scale.value },
-        { translateY: dragOffset.y.value / scale.value },
-
-        // Reset the transform before translating and scaling
-        { translateX: width * (pinchX - 0.5) },
-        { translateY: height * (pinchY - 0.5) },
-
-        { scale: scale.value },
-        { translateX: pinchTransform.x.value / scale.value },
-        { translateY: pinchTransform.y.value / scale.value },
-
-        // Reapply the trasnform after translating and scaling
-        { translateY: height * (0.5 - pinchY) },
-        { translateX: width * (0.5 - pinchX) },
+        { translateX: lastX },
+        { translateX: scaledWidth * (-1 / 2) },
+        { scale: lastScale * eventScale.value },
+        { translateX: width * (1 / 2) },
       ],
     };
-  }, [pinchStart, pinchTransform, layout]);
+  });
+
+  const debugAnimatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: pinchStart.x.value - 10 }],
+    };
+  });
 
   return (
-    <GestureDetector gesture={Gesture.Simultaneous(zoomGesture, dragGesture)}>
+    <GestureDetector gesture={Gesture.Simultaneous(zoomGesture)}>
       <Animated.View onLayout={(e) => setLayout(e.nativeEvent.layout)}>
-        <Animated.View style={animatedStyles}>{children}</Animated.View>
+        <Animated.View style={[animatedStyles, { opacity: 0.5 }]}>
+          {children}
+          <Rect left={0} width={75} color={'red'} />
+          <Rect left={75} width={75} color={'green'} />
+          <Rect left={150} width={75} color={'blue'} />
+          <Rect left={225} width={75} color={'wheat'} />
+        </Animated.View>
+        <Animated.View
+          style={[
+            debugAnimatedStyles,
+            {
+              width: 20,
+              height: 20,
+              borderRadius: 20,
+              backgroundColor: 'yellow',
+              position: 'absolute',
+            },
+          ]}
+        />
       </Animated.View>
     </GestureDetector>
   );
 };
+
+const Rect = ({
+  left,
+  width,
+  color,
+}: {
+  left: number;
+  width: number;
+  color: string;
+}) => (
+  <View
+    style={{
+      position: 'absolute',
+      left: left,
+      top: 0,
+      height: 490,
+      width: width,
+      backgroundColor: color,
+    }}
+  />
+);

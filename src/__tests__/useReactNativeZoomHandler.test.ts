8061520f1da174a1f415226f66575f30c9ff5a0e
Elliott Kember <elliott@openspace.ai>
2022-01-30 10:01:09 -0800

    describe blocks

--- src/__tests__/useReactNativeZoomHandler.test.ts
+++ src/__tests__/useReactNativeZoomHandler.test.ts
@@ -23,81 +23,85 @@ beforeEach(() => {
   animator = result;
 });
 
-it('drags once', () => {
-  act(() => {
-    animator.current.onLayout(layoutEvent);
-    animator.current.onDragUpdate({ translationX: 10, translationY: 10 });
-  });
-
-  expect(animator.current.animatedStyles.transform).toEqual([
-    { translateX: 0 },
-    { translateY: 0 },
-    { scale: 1 },
-    { translateX: 0 },
-    { translateY: 0 },
-    { translateX: 10 },
-    { translateY: 10 },
-  ]);
-});
-
-it('drags twice', () => {
-  act(() => {
-    animator.current.onLayout(layoutEvent);
-    animator.current.onDragUpdate({ translationX: 10, translationY: 10 });
-    animator.current.onDragUpdate({ translationX: -20, translationY: -20 });
-  });
-
-  expect(animator.current.animatedStyles.transform).toEqual([
-    { translateX: 0 },
-    { translateY: 0 },
-    { scale: 1 },
-    { translateX: 0 },
-    { translateY: 0 },
-    { translateX: -20 },
-    { translateY: -20 },
-  ]);
-});
-
-it('scales from the centre', () => {
-  act(() => {
-    animator.current.onLayout(layoutEvent);
-    animator.current.onPinchUpdate({
-      scale: 2,
-      focalX: 50,
-      focalY: 50,
-      numberOfPointers: 2,
+describe('dragging', () => {
+  it('drags once', () => {
+    act(() => {
+      animator.current.onLayout(layoutEvent);
+      animator.current.onDragUpdate({ translationX: 10, translationY: 10 });
     });
+
+    expect(animator.current.animatedStyles.transform).toEqual([
+      { translateX: 0 },
+      { translateY: 0 },
+      { scale: 1 },
+      { translateX: 0 },
+      { translateY: 0 },
+      { translateX: 10 },
+      { translateY: 10 },
+    ]);
   });
 
-  expect(animator.current.animatedStyles.transform).toEqual([
-    { translateX: 50 },
-    { translateY: 50 },
-    { scale: 2 },
-    { translateX: 25 },
-    { translateY: 25 },
-    { translateX: 0 },
-    { translateY: 0 },
-  ]);
+  it('drags twice', () => {
+    act(() => {
+      animator.current.onLayout(layoutEvent);
+      animator.current.onDragUpdate({ translationX: 10, translationY: 10 });
+      animator.current.onDragUpdate({ translationX: -20, translationY: -20 });
+    });
+
+    expect(animator.current.animatedStyles.transform).toEqual([
+      { translateX: 0 },
+      { translateY: 0 },
+      { scale: 1 },
+      { translateX: 0 },
+      { translateY: 0 },
+      { translateX: -20 },
+      { translateY: -20 },
+    ]);
+  });
 });
 
-it('scales from a point', () => {
-  act(() => {
-    animator.current.onLayout(layoutEvent);
-    animator.current.onPinchUpdate({
-      scale: 2,
-      focalX: 75,
-      focalY: 75,
-      numberOfPointers: 2,
+describe('pinching', () => {
+  it('scales from the centre', () => {
+    act(() => {
+      animator.current.onLayout(layoutEvent);
+      animator.current.onPinchUpdate({
+        scale: 2,
+        focalX: 50,
+        focalY: 50,
+        numberOfPointers: 2,
+      });
     });
+
+    expect(animator.current.animatedStyles.transform).toEqual([
+      { translateX: 50 },
+      { translateY: 50 },
+      { scale: 2 },
+      { translateX: 25 },
+      { translateY: 25 },
+      { translateX: 0 },
+      { translateY: 0 },
+    ]);
   });
 
-  expect(animator.current.animatedStyles.transform).toEqual([
-    { translateX: 50 },
-    { translateY: 50 },
-    { scale: 2 },
-    { translateX: 37.5 },
-    { translateY: 37.5 },
-    { translateX: 0 },
-    { translateY: 0 },
-  ]);
+  it('scales from a point', () => {
+    act(() => {
+      animator.current.onLayout(layoutEvent);
+      animator.current.onPinchUpdate({
+        scale: 2,
+        focalX: 75,
+        focalY: 75,
+        numberOfPointers: 2,
+      });
+    });
+
+    expect(animator.current.animatedStyles.transform).toEqual([
+      { translateX: 50 },
+      { translateY: 50 },
+      { scale: 2 },
+      { translateX: 37.5 },
+      { translateY: 37.5 },
+      { translateX: 0 },
+      { translateY: 0 },
+    ]);
+  });
 });

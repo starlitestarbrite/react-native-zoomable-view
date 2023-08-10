df7469cd7b07b2b27505afe9f88b0b2621a6e00c
thomasvo <thomasttvo@gmail.com>
2023-08-10 15:23:34 -0700

    don't build on prepare

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,4 @@
+import ReactNativeZoomableView from './ReactNativeZoomableView';
+import ReactNativeZoomableViewWithGestures from './ReactNativeZoomableViewWithGestures';
+import { ReactNativeZoomableViewProps, ZoomableViewEvent } from './typings';
+export { ReactNativeZoomableView, ReactNativeZoomableViewWithGestures, ReactNativeZoomableViewProps, ZoomableViewEvent, };

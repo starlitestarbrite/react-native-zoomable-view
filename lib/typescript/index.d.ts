47bd472cc9dcd6a4a2d74e57ac98fcfc4379d455
Thomas Vo <thomasttvo@gmail.com>
2023-08-22 12:38:58 -0700

    Don't build on `yarn prepare` (#69)

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,5 @@
+import { applyContainResizeMode, getImageOriginOnTransformSubject, viewportPositionToImagePosition } from './helper/coordinateConversion';
+import ReactNativeZoomableView from './ReactNativeZoomableView';
+import ReactNativeZoomableViewWithGestures from './ReactNativeZoomableViewWithGestures';
+import { ReactNativeZoomableViewProps, ZoomableViewEvent } from './typings';
+export { ReactNativeZoomableView, ReactNativeZoomableViewWithGestures, ReactNativeZoomableViewProps, ZoomableViewEvent, applyContainResizeMode, getImageOriginOnTransformSubject, viewportPositionToImagePosition, };
